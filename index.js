9623bfd24bebeb79a2090a48a3546c6c5dfecc43
OksanaLev26 <137902781+OksanaLev26@users.noreply.github.com>
2023-07-31 14:37:07 -0400

    dom_menu_lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+let showingSubMenu = false;
 const mainEl = document.querySelector('main');
-console.log(mainEl);
 
 mainEl.style.backgroundColor = 'var(--main-bg)';
 
@@ -8,7 +8,6 @@ mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
 mainEl.className = 'flex-ctr';
 
 const topMenuEl = document.getElementById('top-menu');
-console.log(topMenuEl);
 
 topMenuEl.style.height = "100%";
 
@@ -17,11 +16,22 @@ topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 topMenuEl.className = 'flex-around';
 
 let menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 
   menuLinks.forEach((el) => {
     const a = document.createElement('a');
@@ -29,3 +39,72 @@ let menuLinks = [
     a.textContent = el.text;
     topMenuEl.appendChild(a);
   });
+
+  let subMenuEl = document.getElementById('sub-menu');
+  subMenuEl.style.height = '100%';
+  subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
+  subMenuEl.className = 'flex-around';
+  subMenuEl.style.position = 'absolute';
+  subMenuEl.style.top = 0;
+
+  const topMenuLinks = document.querySelectorAll('a');
+
+  const buildSubMenu = (arr) => {
+    subMenuEl.innerHTML = "";
+    arr.forEach((el) => {
+      const a = document.createElement('a');
+      a.href = el.href;
+      a.textContent = el.text;
+      subMenuEl.appendChild(a);
+    });
+  }
+
+  const handleTopMemuElClick = (e) => {
+    e.preventDefault();
+   if (!e.target.href) {
+    return;
+   };
+    console.log(e.target);
+
+    topMenuLinks.forEach((el) => {
+      if (el.textContent !== e.target.textContent) {
+        el.classList.remove('active');
+        showingSubMenu = false;
+        subMenuEl.style.top = 0;
+      }
+    }) 
+   
+    const currentLink = menuLinks.find((el) => el.text === e.target.textContent);
+
+    if (!currentLink.subLinks) {
+      mainEl.textContent = e.target.textContent;
+      showingSubMenu = false;
+    } else {
+      showingSubMenu = true;
+    }
+    
+    e.target.className = 'active';
+      
+    if (showingSubMenu) {
+      buildSubMenu(currentLink.subLinks, subMenuEl);
+      subMenuEl.style.top = '100%';
+    } else {
+      subMenuEl.style.top = 0;
+    }
+  }
+
+  topMenuEl.addEventListener('click', handleTopMemuElClick);
+
+  const handlesubMenuElClick = (e) => {
+    e.preventDefault();
+    if (!e.target.href) {
+      return;
+     };
+    showingSubMenu = false;
+    subMenuEl.style.top = 0;
+
+    topMenuLinks.forEach((el) => el.classList.remove('active'));
+    mainEl.textContent = e.target.textContent;
+  }
+
+  subMenuEl.addEventListener('click', handlesubMenuElClick);
